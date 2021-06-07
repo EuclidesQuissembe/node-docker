cc5de345a0411dec4bedde465d8fc298b952b0b5
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-07 14:47:12 +0100

    :tada: Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect("mongodb://mongo:27017/docker-node-mongo", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const Item = require("./models/Item");
+
+app.get("/", (_, res) => {
+  Item.find()
+    .then((items) => res.render("index", { items }))
+    .catch(() => res.status(404).json({ msg: "No items found" }));
+});
+
+app.post("/item/add", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
+
+  newItem.save().then(() => res.redirect("/"));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log("Server runnig..."));
